9dd4fa0ccf0b0432a6e9d7fb0a40f073cf1fc821
Arjun Patel <arjunpatel9217@gmail.com>
2024-10-02 21:51:52 -0500

    modified the city string request to account for lowercase entries

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -8,7 +8,7 @@ import WeatherService from '../../service/weatherService.js';
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
-  const cityString: string = req.body.cityName; //.split(' ').map((charc: string) => charc.charAt(0).toUpperCase() + charc.slice(1).toLowerCase()).join(' ');
+  const cityString: string = req.body.cityName.split(' ').map((charc: string) => charc.charAt(0).toUpperCase() + charc.slice(1).toLowerCase()).join(' ');
   const nWS = new WeatherService(cityString);
   const newCityWeather = await nWS.getWeatherForCity();
   // TODO: save city to search history
